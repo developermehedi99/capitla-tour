636e0649f9033126549b02e52c86fe721092ab5c
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-10-13 12:54:42 +0600

    rendering commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
    <h1>React simple projects</h1>
+
     </>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import Counter from "./Counter";
 
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Counter></Counter>
             <Outlet></Outlet>
         </div>
     );
